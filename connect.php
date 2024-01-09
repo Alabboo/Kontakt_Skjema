c40d385192f10e5e5fabe1320e6c90e69961fa76
Alabboo <hanafyjonas@gmail.com>
2024-01-09 10:14:17 +0100

    TEst

--- connect.php
+++ connect.php
@@ -1,6 +1,6 @@
 <?php  
     if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST)['submit']){
-       $conn= mysqli_connect('localhost', 'root', 'Jonas1230062004!', 'saker') or die("Connectiong Failed:" .mysqli_connect_error());
+       $conn= mysqli_connect('localhost', 'root', '', 'saker') or die("Connectiong Failed:" .mysqli_connect_error());
         if(isset($_POST['id']) && isset($_POST['name']) && isset($_POST['class']) && isset($_POST['problem'])) {
             $id = $_POST['id'];
             $id = $_POST['name'];
